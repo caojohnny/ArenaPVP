956514161d8b85e2ae304196f8e5e6ecd2804be8
AgentTroll <woodyc40@gmail.com>
2013-08-20 12:25:44 -0700

    Update GameListener.java

--- src/com/gmail/woodyc40/arenapvp/GameListener.java
+++ src/com/gmail/woodyc40/arenapvp/GameListener.java
@@ -4,33 +4,32 @@ package com.gmail.woodyc40.arenapvp;
 
 public class GameListener implements Listener{
 
-  List<String> players = new List<String>();
-  ArenaPVP plugin;
+    List<String> players = new List<String>();
+    ArenaPVP plugin;
   
-  public GameListener(ArenaPVP plugin){
-  	this.plugin = plugin;
-  }
+    public GameListener(ArenaPVP plugin){
+  	  this.plugin = plugin;
+    }
   
-  @EventHandler
-  public void onDamange(EntityDamageByEntityEvent e){
+    @EventHandler
+    public void onDamange(EntityDamageByEntityEvent e){
     
-  }
+    }
   
-  @EventHandler
-  public void onDeath(PlayerDeathEvent e){
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e){
     
-  }
+    }
   
-  public static void add(Player p){
-    final String anme = p.getName();
-    players.add(name);
-    
-    Bukkit.getScheduler().scheduleSyncDelayeddTask(plugin, new Runnable(){
-      @Override
-      public void run(){
-        players.remove(name); 
-      }
-    }, 100L);  
-  }
-
+    public static void add(Player p){
+      final String anme = p.getName();
+      players.add(name);
+      
+      Bukkit.getScheduler().scheduleSyncDelayeddTask(plugin, new Runnable(){
+        @Override
+        public void run(){
+          players.remove(name); 
+        }
+      }, 100L);  
+    }
 }
