0b3aeb9bbbf6956f17684a3d47a03b4d5cc2c31f
AgentTroll <woodyc40@gmail.com>
2013-08-20 12:52:56 -0700

    Update GameListener.java

--- src/com/gmail/woodyc40/arenapvp/GameListener.java
+++ src/com/gmail/woodyc40/arenapvp/GameListener.java
@@ -1,34 +1,46 @@
 package com.gmail.woodyc40.arenapvp;
 
-//imports
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class GameListener implements Listener{
 
-    List<String> players = new List<String>();
-    ArenaPVP plugin;
+    static List<String> players = new ArrayList<String>();
+    static ArenaPVP plugin;
   
     public GameListener(ArenaPVP plugin){
-  	    this.plugin = plugin;
+  	    GameListener.plugin = plugin;
     }
   
     @EventHandler
     public void onDamange(EntityDamageByEntityEvent e){
-    
+    	if(e.getEntity() instanceof Player && players.contains(((Player) e.getEntity()).getName())){
+    		e.setCancelled(true);
+    	}
     }
   
     @EventHandler
     public void onDeath(PlayerDeathEvent e){
-    
+    	if(ArenaManager.getManager().isInGame(e.getEntity())){
+    		ArenaManager.getManager().removePlayer(e.getEntity());
+    	}
     }
   
     public static void add(Player p){
-        final String anme = p.getName();
+        final String name = p.getName();
         players.add(name);
       
-        Bukkit.getScheduler().scheduleSyncDelayeddTask(plugin, new Runnable(){
+        Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable(){
             @Override
             public void run(){
-            players.remove(name); 
+            	players.remove(name); 
             }
         }, 100L);  
     }
